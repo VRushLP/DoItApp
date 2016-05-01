ea01fb476682efbddf5f2e96adb47483b09d42f0
Heyther <heytherp3@gmail.com>
2016-05-01 12:19:22 -0700

    prep to merge

---
+++ app/src/main/java/teamten/tacoma/uw/edu/doit/authenticate/AuthenticationActivity.java
@@ -0,0 +1,15 @@
+package teamten.tacoma.uw.edu.doit.authenticate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import teamten.tacoma.uw.edu.doit.R;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
