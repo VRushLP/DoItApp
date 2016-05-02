91b7c1355dadbe5b901b251937586b9994f2a775
VRushLP <rmfarc@uw.edu>
2016-05-01 22:01:42 -0700

    Reverting

--- app/src/main/java/teamten/tacoma/uw/edu/doit/authenticate/AuthenticationActivity.java
+++ app/src/main/java/teamten/tacoma/uw/edu/doit/authenticate/AuthenticationActivity.java
@@ -1,84 +1,15 @@
 package teamten.tacoma.uw.edu.doit.authenticate;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
-import java.io.OutputStreamWriter;
-
-import teamten.tacoma.uw.edu.doit.DoItStationActivity;
 import teamten.tacoma.uw.edu.doit.R;
 
-public class AuthenticationActivity extends AppCompatActivity implements LogInFragment.LoginInteractionListener, RegistrationFragment.RegistrationInteractionListener{
-
-    private SharedPreferences mSharedPreferences;
+public class AuthenticationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.authentication_activity, new LogInFragment() )
-                .commit();
-
-        mSharedPreferences = getSharedPreferences(getString(R.string.LOGIN_PREFS)
-                , Context.MODE_PRIVATE);
-        if (!mSharedPreferences.getBoolean(getString(R.string.LOGGEDIN), false)) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.authentication_activity, new LogInFragment())
-                    .commit();
-        } else {
-            Intent i = new Intent(this, DoItStationActivity.class);
-            startActivity(i);
-            finish();
-        }
-    }
-
-    @Override
-    public void login(String userId, String pwd) {
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-            //Check if the login and password are valid
-            //new LoginTask().execute(url);
-            try {
-                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
-                        openFileOutput(getString(R.string.LOGIN_FILE)
-                                , Context.MODE_PRIVATE));
-                outputStreamWriter.write("email = " + userId + ";");
-                outputStreamWriter.write("password = " + pwd);
-                outputStreamWriter.close();
-                Toast.makeText(this, "Stored in File Successfully!", Toast.LENGTH_LONG)
-                        .show();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        }
-        else {
-            Toast.makeText(this, "No network connection available. Cannot authenticate user",
-                    Toast.LENGTH_SHORT) .show();
-            return;
-        }
-
-        mSharedPreferences
-                .edit()
-                .putBoolean(getString(R.string.LOGGEDIN), true)
-                .commit();
-
-        Intent i = new Intent(this, DoItStationActivity.class);
-        startActivity(i);
-        finish();
-    }
-
-    @Override
-    public void register() {
-
     }
 }
